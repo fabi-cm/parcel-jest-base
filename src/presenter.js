28618fe82bd22518381ae8a4fec2f417d54f731d
Fabio Copa Mejia <dahofcmmejia@gmail.com>
2023-02-16 06:50:25 -0400

    Funcion multiplicar
    Se agrego la funcion de multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,16 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
+
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+
+const first_m = document.querySelector("#primer-numero-multiplicar");
+const second_m = document.querySelector("#segundo-numero-multiplicar");
+const form_m = document.querySelector("#multiplicar-form");
+const div_m = document.querySelector("#resultado-div-multiplicar");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,3 +20,12 @@ form.addEventListener("submit", (event) => {
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
 });
+
+form_m.addEventListener("submit", (event)=>{
+  event.preventDefault();
+
+  const firstNumberMult = Number.parseInt(first_m.value);
+  const secondNumberMult = Number.parseInt(second_m.value);
+
+  div_m.innerHTML = "<p>" + multiplicar(firstNumberMult, secondNumberMult) + "</p>";
+})
